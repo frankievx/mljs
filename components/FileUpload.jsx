b63ee2cf7888ccfc193f23531195fbe907c1b21c
Francis V <vithayathilf@aetna.com>
2021-08-04 11:49:44 -0600

    handle file input better

--- components/FileUpload.jsx
+++ components/FileUpload.jsx
@@ -1,5 +1,5 @@
 const Uppy = require('@uppy/core')
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import '@uppy/core/dist/style.css'
 import '@uppy/file-input/dist/style.css'
 
@@ -11,35 +11,37 @@ export default function FileUpload({ id, onUpload }) {
     restrictions: { maxNumberOfFiles: 1 },
   })
 
-  uppy.on('file-added', (file) => {
-    console.log('file added')
-    const fileReader = new FileReader();
-    fileReader.onload = ((e) => {
-      onUpload(e)
-      uppy.reset()
-      // element.src = e.target.result;
-    });
-    fileReader.readAsDataURL(file.data);
-  })
+  
 
-  fileInputRef.current.addEventListener('change', (event) => {
-    const file = event.target.files[0]
-    try {
-      uppy.addFile({
-        source: 'file input',
-        name: file.name,
-        type: file.type,
-        data: file,
-      })
-    } catch (err) {
-      if (err.isRestriction) {
-        // handle restrictions
-        console.log('Restriction error:', err)
-      } else {
-        // handle other errors
-        console.error(err)
+  useEffect(() => {
+    uppy.on('file-added', (file) => {
+      const fileReader = new FileReader();
+      fileReader.onload = ((e) => {
+        onUpload(e)
+        uppy.reset()
+        // element.src = e.target.result;
+      });
+      fileReader.readAsDataURL(file.data);
+    })
+    fileInputRef.current.addEventListener('change', (event) => {
+      const file = event.target.files[0]
+      try {
+        uppy.addFile({
+          source: 'file input',
+          name: file.name,
+          type: file.type,
+          data: file,
+        })
+      } catch (err) {
+        if (err.isRestriction) {
+          // handle restrictions
+          console.log('Restriction error:', err)
+        } else {
+          // handle other errors
+          console.error(err)
+        }
       }
-    }
+    })
   })
 
   function handleClick () {
